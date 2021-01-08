838718c97d1dde3a7316e92c51bb3fe38076a9e6
Lê Hải Sơn <sonlh@sonek.vn>
2021-01-08 11:47:25 +0700

    remove stirng 2int

--- String2Int64Service/interface.go
+++
@@ -1,7 +0,0 @@
-package String2Int64Service
-
-type Client interface {
-	PutData(key string, value int64) error
-	GetData(key string) (int64, error)
-	CasData(key string, value int64) (success bool, oldvalue int64, err error)
-}
--- String2Int64Service/s2i64kv/thrift/thriftgen.sh
+++
@@ -1,4 +0,0 @@
-
-# ../../../contribs/ApacheThrift/bin/thrift -r -gen cpp i2skv.thrift
-thrift -r -gen go  s2i64kv.thrift
-
